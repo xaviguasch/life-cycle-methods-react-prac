6ad13f393752de5fd185b56e73b1fc225757473a
xaviguasch <xaviguasch@gmail.com>
2019-05-08 17:36:30 +0200

    added animating loaders

--- src/ZenQuote.js
+++ src/ZenQuote.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './ZenQuote.css'
 
 class ZenQuote extends Component {
   constructor(props) {
     super(props)
-    this.state = { quote: 'hey' }
+    this.state = { quote: '', isLoaded: false }
   }
 
   componentDidMount() {
     axios.get('https://api.github.com/zen').then(response => {
-      this.setState({ quote: response.data })
+      setTimeout(() => {
+        this.setState({ quote: response.data, isLoaded: true })
+      }, 3000)
     })
   }
 
   render() {
     return (
       <div>
-        <h1>Always remember...</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember...</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <div className='loader' />
+        )}
       </div>
     )
   }
