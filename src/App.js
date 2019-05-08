074941322a4d3e6b25d5e201471097be92a47786
xaviguasch <xaviguasch@gmail.com>
2019-05-08 18:25:08 +0200

    loaded data with async functions in componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Timer from './Timer'
 import ZenQuote from './ZenQuote'
+import GithubUserInfo from './GithubUserInfo'
 
 import './App.css'
 
 function App() {
   return (
     <div className='App'>
-      <ZenQuote />
+      <GithubUserInfo username='facebook' />
+      <GithubUserInfo username='colt' />
+      <GithubUserInfo username='sophiebits' />
     </div>
   )
 }
